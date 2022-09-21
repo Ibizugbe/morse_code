e36fec8edc1661d9b53f504a6043271065fe4de8
Lizdev-05 <ojesanmioyinlade@gmail.com>
2022-09-21 14:36:02 +0100

    add decode function

--- morse_code.rb
+++ morse_code.rb
@@ -1,28 +1,3 @@
-# def morse_decode(string)
-#   morse_string {
-#     '.-' => 'a', '-...' => 'b', '-.-.' => 'c', '-..' => 'd', '.' => 'e', '..-.' => 'f', '--.' => 'g',
-#     '....' => 'h', '..' => 'i', '.---' => 'j', '-.-' => 'k', '.-..' => 'l', '--' => 'm', '-.' => 'n',
-#     '---' => 'o', '.--.' => 'p', '--.-' => 'q', '.-.' => 'r', '...' => 's', '-' => 't', '..-' => 'u',
-#     '...-' => 'v', '.--' => 'w', '-..-' => 'x',	'-.--' => 'y', '--..' => 'z'
-#   }
-#   morse_string[string].upcase
-# end
-
-# def morse_function(data)
-#  data.split(' ').map{|v| morse_string[v]}.join
-# end
-
-# def morse_man(data)
-#   data.split('  ').map{|w| morse_string[w]}.join(' ')
-# end
-
-# puts morse_man("-- -.--   -. .- -- .")
-
-# # morse_string = HASH.new()
-# # morse_string = ''
-# # puts for
-
-
 def decode_char(char)
   dictionary = {
     '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
@@ -34,10 +9,10 @@ def decode_char(char)
   dictionary[char]
 end
 
-# def decode_word(word)
-#   char_array = word.split
-#   char_array.map { |x| decode_char(x) }.join
-# end
+def decode_word(word)
+  char_array = word.split
+  char_array.map { |x| decode_char(x) }.join
+end
 
 # def decode_msg(msg)
 #   word_array = msg.split('   ')
