7fdbd3dc1437cfd90d582af19544f3c04e1f283a
Abraham Ibizugbe <abrahamibizugbe@gmail.com>
2022-09-21 14:27:29 +0100

    define object for morse code

--- morse_code.rb
+++ morse_code.rb
@@ -1,10 +1,47 @@
-puts "what is your first name?"
-first_name = gets.chomp
-puts "what is your last name?"
-last_name = gets.chomp
-fullName = "#{first_name} #{last_name}"
-puts "Your full name is #{fullName}"
-reversedName = fullName.reverse
-puts "Your full name reversed is #{reversedName}"
-nameLength = fullName.length
-puts "Your full name length is #{nameLength}"
+# def morse_decode(string)
+#   morse_string {
+#     '.-' => 'a', '-...' => 'b', '-.-.' => 'c', '-..' => 'd', '.' => 'e', '..-.' => 'f', '--.' => 'g',
+#     '....' => 'h', '..' => 'i', '.---' => 'j', '-.-' => 'k', '.-..' => 'l', '--' => 'm', '-.' => 'n',
+#     '---' => 'o', '.--.' => 'p', '--.-' => 'q', '.-.' => 'r', '...' => 's', '-' => 't', '..-' => 'u',
+#     '...-' => 'v', '.--' => 'w', '-..-' => 'x',	'-.--' => 'y', '--..' => 'z'
+#   }
+#   morse_string[string].upcase
+# end
+
+# def morse_function(data)
+#  data.split(' ').map{|v| morse_string[v]}.join
+# end
+
+# def morse_man(data)
+#   data.split('  ').map{|w| morse_string[w]}.join(' ')
+# end
+
+# puts morse_man("-- -.--   -. .- -- .")
+
+# # morse_string = HASH.new()
+# # morse_string = ''
+# # puts for
+
+
+def decode_char(char)
+  dictionary = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G',
+    '....' => 'H', '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N',
+    '---' => 'O', '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T', '..-' => 'U',
+    '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y', '--..' => 'Z'
+  }
+
+  dictionary[char]
+end
+
+# def decode_word(word)
+#   char_array = word.split
+#   char_array.map { |x| decode_char(x) }.join
+# end
+
+# def decode_msg(msg)
+#   word_array = msg.split('   ')
+#   word_array.map { |x| decode_word(x) }.join(' ')
+# end
+
+# puts decode_msg('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
